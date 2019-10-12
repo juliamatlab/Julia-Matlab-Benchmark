035212570b6771d08e1cd0a8984b0c198a4b6116
aminya <aminyahyaabadi74@gmail.com>
2019-10-12 05:00:05 -0500

    Skip changing of number of BLAS threads for MKL

--- JuliaMain.jl
+++ JuliaMain.jl
@@ -12,86 +12,210 @@ using Random # for randperm
 using MAT
 
 const operationMode = 2; # 0 for test only # 1 for partial benchmark # 2 for full benchmark
-const saveData=true; # true for saving Runtimes to csv and mat
-const JuliaFlag=true; # run Julia benchmark
-const JuliaSIMDFlag=true; # run Julia SIMD benchmark
-const debugging=false;
+const saveData = true; # true for saving Runtimes to csv and mat
+const JuliaFlag = true; # run Julia benchmark
+const JuliaSIMDFlag = true; # run Julia SIMD benchmark
+const debugging = false;
 
-# BLAS threads = 1
-BLAS.set_num_threads(1)
-# Julia
-if JuliaFlag
-    include("JuliaBench.jl");
-    tRunTime, mRunTime= JuliaBench(operationMode);
+if BLAS.vendor() == :mkl
+    # MKL
 
-    if saveData
-        # Main RunTime Table write
-        writedlm(joinpath("RunTimeData","RunTimeJulia$(BLAS.vendor())Table.csv"), tRunTime,',');
-        # RunTime save
-        file = matopen(joinpath("RunTimeData","RunTimeJulia$(BLAS.vendor()).mat"), "w")
-        write(file, "mRunTime", mRunTime)
-        close(file)
+    ### Julia
+    if JuliaFlag
+        include("JuliaBench.jl")
+        tRunTime, mRunTime = JuliaBench(operationMode)
+
+        if saveData
+            # Main RunTime Table write
+            writedlm(
+                joinpath(
+                    "RunTimeData",
+                    "RunTimeJulia$(BLAS.vendor())Table.csv",
+                ),
+                tRunTime,
+                ',',
+            )
+            # RunTime save
+            file = matopen(
+                joinpath("RunTimeData", "RunTimeJulia$(BLAS.vendor()).mat"),
+                "w",
+            )
+            write(file, "mRunTime", mRunTime)
+            close(file)
+        end
     end
-end
 
+    ### Julia SIMD
+    if JuliaSIMDFlag
+        include("JuliaBenchSIMD.jl")
+        tRunTime, mRunTime = JuliaBenchSIMD(operationMode)
 
-
-# Julia SIMD
-if JuliaSIMDFlag
-    include("JuliaBenchSIMD.jl")
-    tRunTime, mRunTime= JuliaBenchSIMD(operationMode);
-
-    if saveData
-        # Main RunTime Table write
-        writedlm(joinpath("RunTimeData","RunTimeJulia$(BLAS.vendor())SIMDTable.csv"), tRunTime,',');
-        # RunTime save
-        file = matopen(joinpath("RunTimeData","RunTimeJulia$(BLAS.vendor())SIMD.mat"), "w")
-        write(file, "mRunTime", mRunTime)
-        close(file)
+        if saveData
+            # Main RunTime Table write
+            writedlm(
+                joinpath(
+                    "RunTimeData",
+                    "RunTimeJulia$(BLAS.vendor())SIMDTable.csv",
+                ),
+                tRunTime,
+                ',',
+            )
+            # RunTime save
+            file = matopen(
+                joinpath("RunTimeData", "RunTimeJulia$(BLAS.vendor())SIMD.mat"),
+                "w",
+            )
+            write(file, "mRunTime", mRunTime)
+            close(file)
+        end
     end
-end
 
-# BLAS threads = 4
-BLAS.set_num_threads(4)
-# Julia
-if JuliaFlag
-    include("JuliaBench.jl");
-    tRunTime, mRunTime= JuliaBench(operationMode);
 
-    if saveData
+else
+    # openBLAS
+
+    ## BLAS threads = 1
+    BLAS.set_num_threads(1)
+
+    ### Julia
+    if JuliaFlag
+        include("JuliaBench.jl")
+        tRunTime, mRunTime = JuliaBench(operationMode)
+
+        if saveData
         # Main RunTime Table write
-        writedlm(joinpath("RunTimeData","RunTimeJulia$(BLAS.vendor())Table_4Thread.csv"), tRunTime,',');
+            writedlm(
+                joinpath(
+                    "RunTimeData",
+                    "RunTimeJulia$(BLAS.vendor())Table.csv",
+                ),
+                tRunTime,
+                ',',
+            )
         # RunTime save
-        file = matopen(joinpath("RunTimeData","RunTimeJulia$(BLAS.vendor())_4Thread.mat"), "w")
-        write(file, "mRunTime", mRunTime)
-        close(file)
+            file = matopen(
+                joinpath("RunTimeData", "RunTimeJulia$(BLAS.vendor()).mat"),
+                "w",
+            )
+            write(file, "mRunTime", mRunTime)
+            close(file)
+        end
     end
-end
 
 
 
-# Julia SIMD
-if JuliaSIMDFlag
-    include("JuliaBenchSIMD.jl")
-    tRunTime, mRunTime= JuliaBenchSIMD(operationMode);
+    ### Julia SIMD
+    if JuliaSIMDFlag
+        include("JuliaBenchSIMD.jl")
+        tRunTime, mRunTime = JuliaBenchSIMD(operationMode)
 
-    if saveData
+        if saveData
         # Main RunTime Table write
-        writedlm(joinpath("RunTimeData","RunTimeJulia$(BLAS.vendor())SIMDTable_4Thread.csv"), tRunTime,',');
+            writedlm(
+                joinpath(
+                    "RunTimeData",
+                    "RunTimeJulia$(BLAS.vendor())SIMDTable.csv",
+                ),
+                tRunTime,
+                ',',
+            )
         # RunTime save
-        file = matopen(joinpath("RunTimeData","RunTimeJulia$(BLAS.vendor())SIMD_4Thread.mat"), "w")
-        write(file, "mRunTime", mRunTime)
-        close(file)
+            file = matopen(
+                joinpath("RunTimeData", "RunTimeJulia$(BLAS.vendor())SIMD.mat"),
+                "w",
+            )
+            write(file, "mRunTime", mRunTime)
+            close(file)
+        end
     end
+
+    ## BLAS threads = 4
+    BLAS.set_num_threads(4)
+
+    ### Julia
+    if JuliaFlag
+        include("JuliaBench.jl")
+        tRunTime, mRunTime = JuliaBench(operationMode)
+
+        if saveData
+        # Main RunTime Table write
+            writedlm(
+                joinpath(
+                    "RunTimeData",
+                    "RunTimeJulia$(BLAS.vendor())Table_4Thread.csv",
+                ),
+                tRunTime,
+                ',',
+            )
+        # RunTime save
+            file = matopen(
+                joinpath(
+                    "RunTimeData",
+                    "RunTimeJulia$(BLAS.vendor())_4Thread.mat",
+                ),
+                "w",
+            )
+            write(file, "mRunTime", mRunTime)
+            close(file)
+        end
+    end
+
+
+
+    ### Julia SIMD
+    if JuliaSIMDFlag
+        include("JuliaBenchSIMD.jl")
+        tRunTime, mRunTime = JuliaBenchSIMD(operationMode)
+
+        if saveData
+        # Main RunTime Table write
+            writedlm(
+                joinpath(
+                    "RunTimeData",
+                    "RunTimeJulia$(BLAS.vendor())SIMDTable_4Thread.csv",
+                ),
+                tRunTime,
+                ',',
+            )
+        # RunTime save
+            file = matopen(
+                joinpath(
+                    "RunTimeData",
+                    "RunTimeJulia$(BLAS.vendor())SIMD_4Thread.mat",
+                ),
+                "w",
+            )
+            write(file, "mRunTime", mRunTime)
+            close(file)
+        end
+    end
+
 end
 
 if debugging
     # Debug and performance trace
-    include("JuliaBench.jl");
-    matrixSize = 20;
-    mX = randn(matrixSize, matrixSize);
-    mY = randn(matrixSize, matrixSize);
-    allFunctions = [MatrixGeneration, MatrixAddition, MatrixMultiplication, MatrixQuadraticForm, MatrixReductions, ElementWiseOperations, MatrixExp, MatrixSqrt, Svd, Eig, CholDec, MatInv, LinearSystem, LeastSquares, CalcDistanceMatrix, KMeans];
+    include("JuliaBench.jl")
+    matrixSize = 20
+    mX = randn(matrixSize, matrixSize)
+    mY = randn(matrixSize, matrixSize)
+    allFunctions = [
+        MatrixGeneration,
+        MatrixAddition,
+        MatrixMultiplication,
+        MatrixQuadraticForm,
+        MatrixReductions,
+        ElementWiseOperations,
+        MatrixExp,
+        MatrixSqrt,
+        Svd,
+        Eig,
+        CholDec,
+        MatInv,
+        LinearSystem,
+        LeastSquares,
+        CalcDistanceMatrix,
+        KMeans,
+    ]
     # debug the code
     # Juno.@enter allFunctions[2](matrixSize, mX, mY) # copy paste to REPL
     # timing and memory
